7a40c0851641b464607f702bc96346571132f45b
Alec Saunders <alecsaunders90@gmail.com>
2026-01-17 17:23:03 -0700

    Initial Commit

---
+++ SortAnimation/SortAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortAnimationApp.swift
+//  SortAnimation
+//
+//  Created by Saunders, Alec on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SortAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SortAnimationTests/SortAnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  SortAnimationTests.swift
+//  SortAnimationTests
+//
+//  Created by Saunders, Alec on 1/17/26.
+//
+
+import Testing
+@testable import SortAnimation
+
+struct SortAnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
